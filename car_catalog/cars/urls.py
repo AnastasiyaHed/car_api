bd8c8170c5a84979482f170acfae5dab77a60471
AnastasiyaHed <anastasiya.hedroich@gmail.com>
2023-12-05 22:12:04 +0300

    1 commit

--- car_catalog/cars/urls.py
+++ car_catalog/cars/urls.py
@@ -1,22 +1,16 @@
-# from .views import CarListCreateView, CarDetailView
 # from django.urls import path
-#
+# from .views import *
 #
 # urlpatterns = [
-#     path('cars/', CarListCreateView.as_view(), name='car-list-create'),
-#     path('cars/<int:pk>/', CarDetailView.as_view(), name='car-detail'),
+#     path('', MyModelAPIView.as_view()),
+#     path('search/', SearchView.as_view()),
 # ]
-#
 
 from django.urls import path
-from .views import *
+from .views import MyModelAPIView, SearchView
 
 urlpatterns = [
-    # path('cars/', CarListCreateView.as_view(), name='car-list-create'),
-    # path('cars/<int:pk>/', CarDetailView.as_view(), name='car-detail'),
-    # path('cars/<int:pk>/update/', CarUpdateView.as_view(), name='car-update'),
-    # path('cars/<int:pk>/delete/', CarDeleteView.as_view(), name='car-delete'),
-    path('', MyModelAPIView.as_view()),
-    path('search/', SearchView.as_view()),
+    path('', MyModelAPIView.as_view(), name='car-list-create'),
+    path('<int:pk>/', MyModelAPIView.as_view(), name='car-detail'),
+    path('search/', SearchView.as_view(), name='car-search'),
 ]
-
--- car_catalog/cars/views.py
+++ car_catalog/cars/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from .serializers import MyModelSerializer
 from .models import Car
 from rest_framework.response import Response
+from rest_framework import status
+
 
 class MyModelAPIView(APIView):
     def get(self, request, *args, **kwargs):
@@ -10,9 +12,32 @@ class MyModelAPIView(APIView):
         return Response(serializer.data)
 
 
+    def post(self, request, *args, **kwargs):
+        serializer = MyModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk, *args, **kwargs):
+        car = self.get_object(pk)
+        serializer = MyModelSerializer(car, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, *args, **kwargs):
+        car = self.get_object(pk)
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class SearchView(APIView):
     def get(self, request):
         query = request.query_params.get('q')
         articles = Car.objects.filter(title__icontains=query)
         serializer = MyModelSerializer(articles, many=True)
         return Response(serializer.data)
+
+
